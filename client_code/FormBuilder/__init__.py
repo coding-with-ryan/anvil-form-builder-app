c07e863277dd053700bac4f1b5d595e872626414
ryan@anvil.works <ryan@anvil.works>
2022-12-11 19:58:04 +0000

    Created FormBuilder and FormBuilder.AddComponentForm; Edited settings; Deleted Form1

---
+++ client_code/FormBuilder/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import FormBuilderTemplate
+from anvil import *
+from .AddComponentForm import AddComponentForm
+
+class FormBuilder(FormBuilderTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert(AddComponentForm)
+
